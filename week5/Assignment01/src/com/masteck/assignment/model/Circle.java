ba741fbe2874d5f26d47efb6e4577919aeeee0d3
nikunj1304 <98591289+nikunj1304@users.noreply.github.com>
2022-03-21 09:18:25 +0530

    Java Assignment_01

---
+++ week5/Assignment01/src/com/masteck/assignment/model/Circle.java
@@ -0,0 +1,10 @@
+package com.masteck.assignment.model;
+
+public class Circle implements Shape {
+	 
+    @Override
+    public void draw()
+    {
+        System.out.println("Shape: Circle");
+    }
+}
---
+++ week5/Assignment01/src/com/masteck/assignment/model/RedShapeDecorator.java
@@ -0,0 +1,24 @@
+package com.masteck.assignment.model;
+
+
+
+public class RedShapeDecorator extends ShapeDecorator {
+	
+	public RedShapeDecorator(Shape decoratedShape)
+    {
+        super(decoratedShape);
+    }
+ 
+    @Override public void draw()
+    {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+ 
+    private void setRedBorder(Shape decoratedShape)
+    {
+      // Display message whenever function is called
+        System.out.println("Border Color: Red");
+    }
+
+}
